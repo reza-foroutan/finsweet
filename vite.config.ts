683dd680b4ed499cfb4c3723b5064324b83f95d5
reza-foroutan <rezaforoutanjs@gmail.com>
2025-08-21 15:09:25 +0330

    version-002

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+
+import tailwindcss from "@tailwindcss/vite";
+
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+  server: {
+    port: 5173, // پورت سرور لوکال
+    open: true, // بعد از start مرورگر باز بشه
+  },
+  build: {
+    outDir: "dist", // مسیر خروجی build
+    sourcemap: true, // برای دیباگ
+  },
+  resolve: {
+    alias: {
+      "@": "/src", // استفاده از @ به جای مسیر src
+    },
+  },
+  plugins: [tailwindcss()],
+});
